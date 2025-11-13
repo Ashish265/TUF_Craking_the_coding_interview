bb2b49e0cf74b2d4866fed404f01aa0e345ba10c
Ashish <ashishgupta2692@gmail.com>
2025-11-13 07:59:15 +0530

    optimal solution

--- TUF/array/maximum_subarray_sum.py
+++ TUF/array/maximum_subarray_sum.py
@@ -19,3 +19,21 @@ Output: 1
 Explanation: Array has only one element and which is giving positive sum of 1. 
 
 """
+
+def maximum_sub_array_sum_optimal(arr):
+    n = len(arr)
+    max_sum = float("-inf")
+    current_sum=0
+
+    for i in range(n):
+        current_sum += arr[i]
+        
+        if current_sum > max_sum:
+            max_sum = current_sum
+            
+        if current_sum < 0:
+            current_sum = 0
+        
+    return max_sum
+    
+print(maximum_sub_array_sum_optimal([-2,1,-3,4,-1,2,1,-5,4]))
