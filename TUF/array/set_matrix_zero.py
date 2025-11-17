7b818afb874ded94d20b017ecbe8698848379f90
Ashish <ashishgupta2692@gmail.com>
2025-11-17 23:58:59 +0530

    fix bugs

--- TUF/array/set_matrix_zero.py
+++ TUF/array/set_matrix_zero.py
@@ -18,26 +18,37 @@ Output:[[0,0,0,0],[0,4,5,0],[0,3,1,0]]
 Explanation:Since matrix[0][0]=0 and matrix[0][3]=0. Therefore 1st row, 1st column and 4th column will be set to 0
 """
 
-def matrix(mat):
+def markRow(matrix, n, m, i):
+    # set all non-zero elements as -1 in the row i:
+    for j in range(m):
+        if matrix[i][j] != 0:
+            matrix[i][j] = -1
 
-    row = len(mat)
-    col = len(mat[0])
+def markCol(matrix, n, m, j):
+    # set all non-zero elements as -1 in the col j:
+    for i in range(n):
+        if matrix[i][j] != 0:
+            matrix[i][j] = -1
 
-    for r in range(row):
-        for c in range(col):
-            if mat[r][c] == 0:
-                for i in range(col):
-                    mat[r][i] = '#'
-                for j in range(row):
-                    mat[j][c] = '#'
-
-
-    for i in range(row):
-        for j in range(col):
-            if mat[i][j] == '#':
-                mat[i][j] = 0
-    return mat
+def zeroMatrix(matrix, n, m):
+    # Set -1 for rows and cols
+    # that contains 0. Don't mark any 0 as -1:
+    for i in range(n):
+        for j in range(m):
+            if matrix[i][j] == 0:
+                markRow(matrix, n, m, i)
+                markCol(matrix, n, m, j)
+    
+    # Finally, mark all -1 as 0:
+    for i in range(n):
+        for j in range(m):
+            if matrix[i][j] == -1:
+                matrix[i][j] = 0
+    
+    return matrix
 
 # Example usage
-mat = [[1,1,0],[1,0,1],[1,1,1]]
-print(matrix(mat))  # Output: [[1,0,1],[0,0,0],[1,0,1]]
+matt = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+n = len(matt)
+m = len(matt[0])
+print(zeroMatrix(matt,n,m))  # Output: [[1,0,1],[0,0,0],[1,0,1]]
