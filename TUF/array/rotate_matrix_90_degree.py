c4a0158c315c1da8ee8ebafde657a7365e3be37d
Ashish <ashishgupta2692@gmail.com>
2025-11-19 23:59:27 +0530

    new examples

--- TUF/array/rotate_matrix_90_degree.py
+++ TUF/array/rotate_matrix_90_degree.py
@@ -35,3 +35,4 @@ def rotateMatrix(mat):
 # Example usage
 matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
 print(rotateMatrix(matrix))  # Output:[[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+print(rotateMatrix([[1,2,3],[4,5,6],[7,8,9]]))  # Output: [[7,4,1],[8,5,2],[9,6,3]]
