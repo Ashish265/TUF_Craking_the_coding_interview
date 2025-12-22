c7308ebee90138edc36e8417686262a972b5f65f
Ashish <ashishgupta2692@gmail.com>
2025-12-22 22:09:54 +0530

    linear

---
+++ TUF/binary_search/peak_element.py
@@ -0,0 +1,41 @@
+"""
+Docstring for TUF.binary_search.peak_element
+Given an array of length N, peak element is defined as the element greater than both of its neighbors.
+Formally, if arr[i] is the peak element, arr[i - 1] < arr[i] and arr[i + 1] < arr[i]. 
+Find the index(0-based) of a peak element in the array. If there are multiple peak numbers, return the index of any peak number.
+
+Input: arr[] = {1,2,3,4,5,6,7,8,5,1}
+Output: 7
+Explanation: There is only 1 peak element, 8,  that is at index 7.
+Input: arr[] = {1,2,1,3,5,6,4} 
+Output: 1 
+Explanation : There are 2 peak numbers that are at indices 1 and 5. We can return any of them.
+
+"""
+def findPeakElement(arr):
+    n = len(arr)
+
+    for i in range(1, n - 1):
+        if arr[i] > arr[i - 1] and arr[i] > arr[i + 1]:
+            return i
+    # Check for peak at the beginning
+    if n > 1 and arr[0] > arr[1]:
+        return 0
+    # Check for peak at the end
+    if n > 1 and arr[n - 1] > arr[n - 2]:
+        return n - 1
+    return -1  # No peak found (should not happen for valid input)
+
+# Driver code
+if __name__ == "__main__":
+    arr = [1, 2, 3, 4, 5, 6, 7, 8, 5, 1]
+    index = findPeakElement(arr)
+    print("The index of a peak element is:", index) # Output: 7
+
+    arr = [1, 2, 1, 3, 5, 6, 4]
+    index = findPeakElement(arr)
+    print("The index of a peak element is:", index) # Output: 7
+
+    arr = [100, 20, 15, 2, 2, 1, 0]
+    index = findPeakElement(arr)
+    print("The index of a peak element is:", index) # Output: 5
